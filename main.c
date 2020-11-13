aab24eb760a16402254d9e1a5dd1546a68b53c89
kulevkirill <74400138+kulevkirill@users.noreply.github.com>
2020-11-13 21:24:28 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-int main()
+int main(int argc, char** argv, char** envp)
 {
-	return 0;
+        char** new_envp = (char**) calloc(argc - 1, sizeof(char));
+
+        for (int i = 0; i < argc - 2; i++)
+                new_envp[i] = argv[i + 2];
+
+        execve(argv[1], argv + 1, new_envp);
+
+        return 0;
 }
